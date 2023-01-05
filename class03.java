402340ddd2c10106fab3db72afad0a61136f94c4
ggmanok <105347919+ggmanok@users.noreply.github.com>
2023-01-06 01:00:13 +0800

    Add files via upload

---
+++ class03.java
@@ -0,0 +1,20 @@
+package java14;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class class03{
+   public static void main(String args[]) throws IOException{
+      int n,sum=0;
+      BufferedReader buf;
+      String str;
+      buf=new BufferedReader(new InputStreamReader(System.in));
+      
+      System.out.print("¿é¤J¾ã¼Æ:");
+      str=buf.readLine();
+      n=Integer.parseInt(str);
+
+      for(int i=1;i<=n;i++)
+         sum+=i;
+      System.out.println("1+2+...+"+n+"="+sum);
+   }
+}
